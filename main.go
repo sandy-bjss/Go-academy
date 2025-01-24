aa5a41dca8e0c5c0fd5b34c9ddde211ce9da2e10
Sandy Lamond <sandy.lamond@bjss.com>
2025-01-24 12:24:28 +0000

    added a channel to listen for an OS interupt signal so that the app stays open until user does a keyboard interrupt

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+
 	todo "sandy.goacademy/todo/cli"
 )
 
 func main() {
 
-	todo.TodoCli()
+	// create a channel for waiting for signal from OS
+	sigs := make(chan os.Signal, 1)
 
+	// notify the channel of a signal from the OS
+	signal.Notify(sigs, os.Interrupt)
+
+	// run cli
+	todo.TodoCli()
+	fmt.Println("==================\ncli running: CTRL-C to exit")
+
+	// capture signal
+	sig := <-sigs
+	fmt.Println("Keyboard interupt: " + sig.String() + "\nClosing todo app")
 }
