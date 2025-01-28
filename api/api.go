2afdd08ef08104a717433648597ad89970813f1f
Sandy Lamond <sandy.lamond@bjss.com>
2025-01-28 14:00:25 +0000

    updated traceID middleware for adding traceID to requests

--- api/api.go
+++ api/api.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
 const PORT = ":8080"
 
-func Api() {
+// const for traceID key of TraceIDType
+const TraceIDString = TraceIDType("traceID")
 
+type TraceIDType string
+
+func Api() {
 	// basic logging
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
@@ -19,6 +26,8 @@ func Api() {
 	mux := http.NewServeMux()
 
 	// define handlers for routing
+	gh := http.HandlerFunc(genericHandler)
+	mux.Handle("/", middlewareOne(middlewareTwo(middlewareTraceID(gh))))
 	mux.HandleFunc("/create", genericHandler)
 	mux.HandleFunc("/get", genericHandler)
 	mux.HandleFunc("/update", genericHandler)
@@ -32,8 +41,31 @@ func Api() {
 	slog.Info("Server Started, listening...", "PORT", PORT)
 }
 
+func middlewareOne(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("executing middleware 1")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func middlewareTwo(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("executing middleware 2")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func middlewareTraceID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("middlewareTRaceID: adding context traceID")
+		r.WithContext(context.WithValue(r.Context(), TraceIDString, uuid.New()))
+		next.ServeHTTP(w, r)
+	})
+}
+
 func genericHandler(w http.ResponseWriter, r *http.Request) {
+	slog.Info("executing generic handler", string(TraceIDString), r.Context())
 	w.Header().Set("Content-Type", "application/json")
 	endpoint := strings.TrimPrefix(r.URL.Path, "")
-	fmt.Fprintf(w, "Api endpoint %s", endpoint)
+	fmt.Fprintf(w, "Api endpoint: %s\nEndpoint type: %v", endpoint, w.Header().Get("Content-Type"))
 }
